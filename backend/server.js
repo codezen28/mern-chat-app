9bcd5c84233fb60f61636d1ce9a393011df9e263
codezen28 <128896831+codezen28@users.noreply.github.com>
2023-08-07 11:41:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path=require("path");
 
 const app = express();
 
@@ -18,13 +19,33 @@ const PORT = process.env.BASE_URL || 5000;
 //app.use(cors);
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
+
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+////////Deployment
+
+if(process.env.NODE_ENV === "production")
+{
+  app.use(express.static(path.join(__dirname1,"/frontend/build")));
+
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
+})
+}
+else{
+  app.get("/", (req, res) => {
+  res.send("API is running successfully");
+    
+});
+}
+
+
+
+
+/////Deployment
 
 app.use(notFound);
 app.use(errorHandler);
